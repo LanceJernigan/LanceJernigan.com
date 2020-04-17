6c856ccb5becd7eb6e3602ee6d78608790b5a3a7
Lance Jernigan <Lance@LanceJernigan.com>
2020-04-17 16:38:08 -0400

    Update social links

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -17,17 +17,17 @@ const Profile = () => (
       <h2>Lead Web Engineer</h2>
       <ul className="profile__social">
         <li>
-          <a href="twitter.com" target="_blank">
+          <a href="https://www.twitter.com/lance_jernigan" target="_blank">
             <TwitterIcon />
           </a>
         </li>
         <li>
-          <a href="twitter.com" target="_blank">
+          <a href="https://www.linkedin.com/in/lancejernigan" target="_blank">
             <LinkedInIcon />
           </a>
         </li>
         <li>
-          <a href="twitter.com" target="_blank">
+          <a href="https://www.github.com/lancejernigan" target="_blank">
             <GitHubIcon />
           </a>
         </li>

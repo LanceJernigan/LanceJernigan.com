d91c4cca36d7fb160126df51f1bfb95930235f8b
Lance Jernigan <Lance@LanceJernigan.com>
2020-04-21 18:04:14 -0400

    Add resources template support

---
+++ src/pages/resources/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Sheet from "../../components/sheet";
+import Hero from "../../components/hero";
+
+const Resources = () => (
+  <section className="home">
+    <Hero>
+      <h1>Resources</h1>
+    </Hero>
+
+    <Sheet className="content">
+      <h2>Coming Soon</h2>
+    </Sheet>
+  </section>
+);
+
+export default Resources;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/home";
 import Resource from "./components/resource";
+import Resources from "./pages/resources";
 import FourOhFour from "./pages/fourOhFour";
 
 const Routes = () => (
@@ -11,6 +12,9 @@ const Routes = () => (
     <Switch>
       <Route path="/resource/:slug">
         <Resource />
+      </Route>
+      <Route path="/resources">
+        <Resources />
       </Route>
       <Route path="/about">
         <About />

36ceb141efa7980ac92e8b5d1f71b71b04350d0a
Lance Jernigan <Lance@LanceJernigan.com>
2020-05-01 15:15:37 -0400

    Add sticky header shadows

--- src/components/stickyHeader/index.js
+++ src/components/stickyHeader/index.js
@@ -3,9 +3,13 @@ import React from "react";
 import "./style.scss";
 
 const StickyHeader = ({ children }) => (
-  <header className="sticky_header">
-    <section className="sticky_header__inner">{children}</section>
-  </header>
+  <>
+    <header className="sticky_header">
+      <section className="sticky_header__inner">{children}</section>
+    </header>
+    <div className="sticky_header__shadow" />
+    <div className="sticky_header__cover" />
+  </>
 );
 
 export default StickyHeader;

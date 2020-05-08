60b03125630a956463d3d7ad9075adffa4a1b01a
Lance Jernigan <Lance@LanceJernigan.com>
2020-05-08 15:43:52 -0400

    Add other resources

--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import codeSplittingCoverSrc from "../../assets/codeSplittingCover.jpg";
+import betterAppsCoverSrc from "../../assets/betterAppsCover.jpg";
+import wordPressReactCoverSrc from "../../assets/wordPressReactCover.jpg";
 
 import Sheet from "../../components/sheet";
 import Hero from "../../components/hero";
@@ -22,20 +24,20 @@ const Resources = () => (
         <h2>Featured</h2>
       </StickyHeader>
       <section className="resources__featured">
-        <Card background={codeSplittingCoverSrc}>
+        <Card background={betterAppsCoverSrc}>
           <h2>
             <a
-              href="https://www.linkedin.com/posts/lancejernigan_code-splitting-and-asynchronous-components-activity-6655610663571992576-hAX6"
+              href="https://www.linkedin.com/posts/lancejernigan_building-better-apps-with-javascript-activity-6655611066845921280-MTek"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Code Splitting and Asynchronous Components
+              Building Better Apps with Javascript
             </a>
           </h2>
         </Card>
       </section>
 
-      {/* <StickyHeader>
+      <StickyHeader>
         <h2>All</h2>
       </StickyHeader>
       <ul className="resources__all">
@@ -53,45 +55,19 @@ const Resources = () => (
           </Card>
         </li>
         <li>
-          <Card background={codeSplittingCoverSrc}>
+          <Card background={wordPressReactCoverSrc}>
             <h2>
               <a
-                href="https://www.linkedin.com/posts/lancejernigan_code-splitting-and-asynchronous-components-activity-6655610663571992576-hAX6"
+                href="https://www.linkedin.com/posts/lancejernigan_building-web-apps-with-wordpress-and-react-activity-6655608445674360832-kkNy"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Code Splitting and Asynchronous Components
+                Building Web Apps with WordPress and React
               </a>
             </h2>
           </Card>
         </li>
-        <li>
-          <Card background={codeSplittingCoverSrc}>
-            <h2>
-              <a
-                href="https://www.linkedin.com/posts/lancejernigan_code-splitting-and-asynchronous-components-activity-6655610663571992576-hAX6"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Code Splitting and Asynchronous Components
-              </a>
-            </h2>
-          </Card>
-        </li>
-        <li>
-          <Card background={codeSplittingCoverSrc}>
-            <h2>
-              <a
-                href="https://www.linkedin.com/posts/lancejernigan_code-splitting-and-asynchronous-components-activity-6655610663571992576-hAX6"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Code Splitting and Asynchronous Components
-              </a>
-            </h2>
-          </Card>
-        </li>
-      </ul> */}
+      </ul>
     </Sheet>
   </section>
 );

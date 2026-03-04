b095877b176f96eee48994e35012d841ed56e064
LanceJernigan <lance@lancejernigan.com>
2026-03-04 15:39:33 -0500

    Improve best practices

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
           muted={true}
           autoPlay={true}
           loop={true}
-          poster="https://images.pexels.com/videos/7565449/pexels-photo-7565449.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          src="https://videos.pexels.com/video-files/7565449/7565449-hd_1920_1080_25fps.mp4"
+          poster="/assets/images/hero-poster.jpeg"
+          src="/assets/videos/hero-video.mp4"
           preload="metadata"
         />
         <Hero />

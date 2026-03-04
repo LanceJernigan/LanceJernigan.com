29a8a63c07707e6809f1e8dc9a35a032658751c8
LanceJernigan <lance@lancejernigan.com>
2026-03-04 15:12:49 -0500

    Better accessibility

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,45 +5,48 @@ import GitHub from "@/icons/github";
 import Dribbble from "@/icons/dribbble";
 
 const Footer = () => (
-	<footer className={styles.component}>
-		<div className={styles.wrapper}>
-			<ul className={styles.socials}>
-				<li>
-					<a
-						href="https://www.linkedin.com/in/lancejernigan/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<LinkedIn />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://x.com/Lance_Jernigan"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<X />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://github.com/LanceJernigan/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<GitHub />
-					</a>
-				</li>
-				{/* <li>
+  <footer className={styles.component}>
+    <div className={styles.wrapper}>
+      <ul className={styles.socials}>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/lancejernigan/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="LinkedIn Profile"
+          >
+            <LinkedIn />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://x.com/Lance_Jernigan"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="X (Twitter) Profile"
+          >
+            <X />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/LanceJernigan/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="GitHub Profile"
+          >
+            <GitHub />
+          </a>
+        </li>
+        {/* <li>
 					<a href="">
 						<Dribbble />
 					</a>
 				</li> */}
-			</ul>
-			<p className={styles.copy}>© {new Date().getFullYear()} Lance Jernigan</p>
-		</div>
-	</footer>
+      </ul>
+      <p className={styles.copy}>© {new Date().getFullYear()} Lance Jernigan</p>
+    </div>
+  </footer>
 );
 
 export default Footer;
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -5,53 +5,56 @@ import GitHub from "@/icons/github";
 import Dribbble from "@/icons/dribbble";
 
 export default function Hero() {
-	return (
-		<section className={styles.hero}>
-			<div className={styles.wrapper}>
-				<header className={styles.header}>
-					<h1 className={styles.title}>Lance Jernigan</h1>
-					<h2 className={styles.subtitle}>Product Engineer</h2>
-					<p>
-						I partner with teams to build scalable, user-centered web products,
-						from net new ideas to iterative improvements, across design,
-						engineering, and analytics.
-					</p>
-				</header>
-				<ul className={styles.socials}>
-					<li>
-						<a
-							href="https://www.linkedin.com/in/lancejernigan/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<LinkedIn />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://x.com/Lance_Jernigan"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<X />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/LanceJernigan/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<GitHub />
-						</a>
-					</li>
-					{/* <li>
+  return (
+    <section className={styles.hero}>
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Lance Jernigan</h1>
+          <h2 className={styles.subtitle}>Product Engineer</h2>
+          <p>
+            I partner with teams to build scalable, user-centered web products,
+            from net new ideas to iterative improvements, across design,
+            engineering, and analytics.
+          </p>
+        </header>
+        <ul className={styles.socials}>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/lancejernigan/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="LinkedIn Profile"
+            >
+              <LinkedIn />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://x.com/Lance_Jernigan"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="X (Twitter) Profile"
+            >
+              <X />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/LanceJernigan/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="GitHub Profile"
+            >
+              <GitHub />
+            </a>
+          </li>
+          {/* <li>
 						<a href="">
 							<Dribbble />
 						</a>
 					</li> */}
-				</ul>
-			</div>
-		</section>
-	);
+        </ul>
+      </div>
+    </section>
+  );
 }
